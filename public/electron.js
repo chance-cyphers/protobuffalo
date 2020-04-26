3fbafffd120b4814587f9538986abfee0a929c9c
chance <chance.a.cyphers@gmail.com>
2020-04-26 16:51:22 -0400

    add ci

--- public/electron.js
+++ public/electron.js
@@ -31,9 +31,9 @@ function createWindow() {
 app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+    // if (process.platform !== 'darwin') {
+    //     app.quit()
+    // }
 });
 
 app.on('activate', () => {
