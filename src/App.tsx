ff7cbd1ce6a0fcb5067a46390d15fe6e1c289738
chance <chance.a.cyphers@gmail.com>
2021-05-04 13:01:43 -0400

    make everything beautiful

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,90 @@ import React from 'react';
 import './App.css';
 import ProtoView from "./components/protoview/protoview";
 import {loadProtoClicked} from "./actions/actions";
+import {createMuiTheme, Divider, Grid, makeStyles, ThemeProvider} from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 import {connect} from "react-redux";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#6ab7ff',
+      main: '#1e88e5',
+      dark: '#005cb2'
+    },
+    secondary: {
+      main: '#8d6e63',
+      light: '#be9c91',
+      dark: '#5f4339'
+    },
+    background: {
+      default: '#005cb2'
+    },
+    type: "light"
+  },
+});
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'center',
+    color: '#eee'
+  },
+  title: {
+    padding: theme.spacing(2),
+  },
+  protoList: {
+    backgroundColor: theme.palette.primary.dark
+  },
+  protoListItem: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+    borderWidth: "10px",
+    borderColor: "red"
+  }
+}));
+
 
 function App(props: any) {
 
-    function handleClick() {
-        props.loadProtoClicked();
-    }
+  const classes = useStyles();
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                Hallo
-                <button onClick={handleClick}>Load Proto</button>
-                <ProtoView/>
-            </header>
-        </div>
-    );
+  function handleClick() {
+    props.loadProtoClicked();
+  }
+
+  return (
+      <ThemeProvider theme={theme}>
+        <Grid container className={classes.root}>
+
+          <Grid item xs={12}>
+            <Box bgcolor="primary.main" className={classes.title}>
+              Proto Buffalo
+            </Box>
+          </Grid>
+
+          <Grid item xs={3}>
+            <div className={classes.protoList}>
+              <div className={classes.protoListItem}>Protos</div>
+              <Divider/>
+              <Divider/>
+              <Divider/>
+              <div className={classes.protoListItem} onClick={handleClick}>new</div>
+              <Divider/>
+              <div className={classes.protoListItem}>My Proto</div>
+              <Divider/>
+              <div className={classes.protoListItem}>Some Other Proto</div>
+            </div>
+          </Grid>
+
+          <Grid item xs={9}>
+            {/*<Button variant="contained" onClick={handleClick}>Load Proto</Button>*/}
+            <ProtoView/>
+          </Grid>
+
+        </Grid>
+      </ThemeProvider>
+  );
 }
 
-export default connect(null, { loadProtoClicked })(App);
+export default connect(null, {loadProtoClicked})(App);
--- src/components/protoview/protoview.tsx
+++ src/components/protoview/protoview.tsx
@@ -1,16 +1,34 @@
 import React, {ChangeEvent} from 'react';
 import {connect} from "react-redux";
-import {jsonBodyChanged, methodSelected, rpcInvoked, serverAddressChanged, serviceSelected} from "../../actions/actions";
+import {
+  jsonBodyChanged,
+  methodSelected,
+  rpcInvoked,
+  serverAddressChanged,
+  serviceSelected
+} from "../../actions/actions";
 import {Method, Service} from "protobufjs";
 import JsonInput from "../jsoninput/jsoninput";
+import {Divider, Grid, InputLabel, makeStyles, MenuItem, Select, TextField} from "@material-ui/core";
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    textAlign: 'left',
+    color: 'black'
+  }
+}));
+
 
 const ProtoView = (props: any) => {
 
-  function handleServiceChange(event: ChangeEvent<HTMLSelectElement>) {
+  const classes = useStyles();
+
+  function handleServiceChange(event: ChangeEvent<any>) {
     props.serviceSelected(event.target.value);
   }
 
-  function handleMethodChange(event: ChangeEvent<HTMLSelectElement>) {
+  function handleMethodChange(event: ChangeEvent<any>) {
     props.methodSelected(event.target.value);
   }
 
@@ -23,29 +41,68 @@ const ProtoView = (props: any) => {
   }
 
   return (
-      <div>
-        <input type="text" value={props.serverAddress} onChange={handleServerAddrChanged}/>
-        <br/>
-        <select onChange={handleServiceChange}>
-          {props.services.map((s: Service) => {
-            return (<option key={s.name}>{s.name}</option>)
-          })}
-        </select>
-        <select onChange={handleMethodChange}>
-          {
-            props.selectedService ?
-                props.selectedService.methodsArray.map((m: Method) => {
-                  return (<option key={m.name}>{m.name}</option>)
-                })
-                : ""
-          }
-        </select>
-        <br/>
-        <JsonInput/>
+      <Grid container className={classes.root}>
+
+        <Grid item xs={12}>
+          My Proto
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField id="outlined-basic" label="URL" variant="filled" value={props.serverAddress}
+                     onChange={handleServerAddrChanged}/>
+        </Grid>
+
+        <Grid item xs={6}>
+          <InputLabel id="service-label">Service</InputLabel>
+          <Select
+              labelId="service-label"
+              onChange={handleServiceChange}
+              value={props.selectedService ? props.selectedService.name : ""}
+          >
+            {props.services.map((s: Service) => {
+              return (<MenuItem value={s.name} key={s.name}>{s.name}</MenuItem>)
+            })}
+          </Select>
+        </Grid>
+
+        <Grid item xs={6}>
+          <InputLabel id="method-label">Method</InputLabel>
+          <Select
+              labelId="method-label"
+              onChange={handleMethodChange}
+              value={props.selectedMethod ? props.selectedMethod.name : ""}
+          >
+            {
+              props.selectedService ?
+                  props.selectedService.methodsArray.map((m: Method) => {
+                    return (<MenuItem value={m.name} key={m.name}>{m.name}</MenuItem>)
+                  })
+                  : null
+            }
+          </Select>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Divider/>
+        </Grid>
+
+        <Grid item xs={3}>TLS</Grid>
+        <Grid item xs={3}>Request (form)</Grid>
+        <Grid item xs={3}>Request (JSON)</Grid>
+        <Grid item xs={3}>Response</Grid>
+
+        <Grid item xs={12}>
+          <Divider/>
+        </Grid>
+
+        <Grid item xs={12}>
+          <JsonInput/>
+        </Grid>
+
         <button onClick={handleInvoke} disabled={!props.selectedService || !props.selectedMethod}>Invoke</button>
         <p>Response: </p>
         <p>{props.response}</p>
-      </div>
+      </Grid>
   )
 };
 
